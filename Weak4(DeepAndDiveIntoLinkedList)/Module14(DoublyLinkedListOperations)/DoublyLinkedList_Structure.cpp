e965335a20de396288c27527dfada0268816f726
Sheikh Md. Mahedi Hasan <78357062+Mahedi-Hasan10@users.noreply.github.com>
2023-01-31 22:50:34 +0600

    Add files via upload

---
+++ Weak4(DeepAndDiveIntoLinkedList)/Module14(DoublyLinkedListOperations)/DoublyLinkedList_Structure.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+};
+class DoublyLinkedList
+{
+public:
+    node *head;
+    int sz;
+    DoublyLinkedList()
+    {
+        head = NULL;
+        sz = 0;
+    }
+
+};
+
+int main()
+{
+
+    return 0;
+}
