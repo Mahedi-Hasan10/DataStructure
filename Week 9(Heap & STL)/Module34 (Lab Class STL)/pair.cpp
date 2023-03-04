7fdfb27abd4f222e7241ba513c28d119a603b2cf
Sheikh Md. Mahedi Hasan <78357062+Mahedi-Hasan10@users.noreply.github.com>
2023-03-04 13:30:26 +0600

    Add files via upload

---
+++ Week 9(Heap & STL)/Module34 (Lab Class STL)/pair.cpp
@@ -0,0 +1,16 @@
+/*
+     Author : Mahedi-Hasan10
+     Date : 04-03-2023
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<string, int> student;
+
+    student.first = "Mahedi Hasan";
+    student.second = 438820;
+
+    cout << "Student Name : " << student.first << "\nRoll : " << student.second << "\n";
+    return 0;
+}
